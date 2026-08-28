a78dd5c69ddb17f716dcc995c506148055bb6165
Sakthi-PC <mssakthieesvar@gmail.com>
2026-08-28 10:56:28 +0800

    Stream dataset from hugging face

--- image_loader_batch.py
+++ image_loader_batch.py
@@ -1,4 +1,9 @@
 from datasets import load_dataset
 
-train_dataset = load_dataset("saberzl/SID_Set", split="train")
-val_dataset = load_dataset("saberzl/SID_Set", split="val")
+repo_id = "saberzl/SID_Set"
+
+train_dataset = load_dataset(repo_id, split="train", streaming = True)
+val_dataset = load_dataset(repo_id, split="validation", streaming = True)
+
+for sample in train_dataset:
+    print(sample)
